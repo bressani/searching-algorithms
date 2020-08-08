78e99b29c0c7c1fde9bf6ebaea457876e0699f04
bressani <jr.bressani@gmail.com>
2020-08-07 21:06:42 -0300

    Refactor

--- src/main/kotlin/RecursiveBinarySearch.kt
+++ src/main/kotlin/RecursiveBinarySearch.kt
@@ -1,6 +1,5 @@
 fun main() {
     val array = intArrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
-//    val array = intArrayOf(0, 1, 2, 3)
 
     val numberIndex = recursiveBinarySearch(10, array)
 
